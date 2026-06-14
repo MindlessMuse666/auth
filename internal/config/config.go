943e1df0a5f353d1352ed0a8ce5298791d9d774b
vlad <mindlessmuse.666@gmail.com>
2026-06-14 11:24:33 +0300

    feat(config): описан базовый auth-конфиг

---
+++ internal/config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string        `toml:"env" env-default:"prod"`
+	StoragePath string        `toml:"storage_path" env-required:"true"`
+	TokenTTL    time.Duration `toml:"token_ttl"  env-required:"true"`
+	GRPCConfig  GRPCConfig    `toml:"grpc"`
+}
+
+type GRPCConfig struct {
+	Port    int           `toml:"port"`
+	Timeout time.Duration `toml:"timeout"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+	if path == "" {
+		panic("Путь к конфигу не может быть пустым")
+	}
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("Конфиг-файла не существует: " + path)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("Ошибка чтения конфига: " + err.Error())
+	}
+
+	return &cfg
+}
+
+func fetchConfigPath() string {
+	var res string
+
+	// --config="path/to/config.toml"
+	flag.StringVar(&res, "config", "", "Путь до конфиг-файла")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
